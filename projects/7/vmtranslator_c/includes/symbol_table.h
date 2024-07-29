f4c98b77e323c4a378a578e00088c54da4c8defc
hyacinth <buddhamulberry@gmail.com>
2024-07-29 15:36:52 +0800

    mid-7cpp

---
+++ projects/7/vmtranslator_c/includes/symbol_table.h
@@ -0,0 +1,6 @@
+#ifndef __SYMBOL_TABLE_H_
+#define __SYMBOL_TABLE_H_
+
+
+
+#endif
--- projects/7/vmtranslator_cpp/modules/parse.cpp
+++ projects/7/vmtranslator_cpp/modules/parse.cpp
@@ -1,16 +1,23 @@
 module;
 
+#include <string>
 #include <string_view>
+#include <fstream>
 #include <ranges>
 
 export module parse;
 
-export enum command_type_t : unsigned char {
-    C_ARITHETIC, C_PUSH, C_POP, C_LABEL,
-    C_GOTO, C_IF, C_FUNCTION, C_RETURN, C_CALL,
+export namespace parse {
+enum command_type_t : unsigned char {
+    C_BLANK, 
+    C_ARITHMETIC,
+    C_PUSH,
+    C_POP,
+    C_LABEL,
+    C_GOTO,
+    C_IF,
+    C_FUNCTION,
+    C_RETURN,
+    C_CALL,
 };
-
-export struct parse_t {
-private :
-    
 }
